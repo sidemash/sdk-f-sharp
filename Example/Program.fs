e890bf53ee5454afa8b5ce9afaf34340c681275f
Amadou <amadou.beidari@sidemash.com>
2021-06-29 12:41:29 +0200

    restructure project

---
+++ Example/Program.fs
@@ -0,0 +1,14 @@
+﻿open Sidemash.Sdk
+          
+[<EntryPoint>]
+let main argv =
+    let auth = Auth.make "1342" "****"
+    let sdm = Client.make auth
+
+    // let domain = sdm.Domain.Create(CreateDomainForm.make "example695.com" Domain.Play (Some "My example domain name") None)
+
+    // printfn "%s" (domain.ToString())
+    // printfn "%s" (Some(Domain.Play).ToString())
+    // printfn "%s" (Some(Domain.Play).ToString())
+    printfn "%s" (auth.ToString())
+    0 // return an integer exit code
--- src/Example/Program.fs
+++
@@ -1,7 +0,0 @@
-﻿open Sidemash.Sdk
-          
-[<EntryPoint>]
-let main argv =
-    
-    printfn "%s" ("Hello World")
-    0 // return an integer exit code
